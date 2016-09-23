85f500a3af3376b0329c36228fd150ccc07f2d24
redacid <redacid@redacid.org.ua>
2016-09-23 12:49:49 +0300

    dev

---
+++ myfu/myfu.go
@@ -0,0 +1,75 @@
+package myfu
+
+import (
+	//"fmt"
+	"regexp"
+	"os"
+	"bufio"
+	"fmt"
+)
+
+func Round(f float64) int {
+	if f < -0.5 {
+		return int(f - 0.5)
+	}
+	if f > 0.5 {
+		return int(f + 0.5)
+	}
+	return 0
+}
+
+func Grep(re, filename string) string {
+	regex, err := regexp.Compile(re)
+	if err != nil {
+		return "there was a problem with the regular expression"
+	}
+
+	fh, err := os.Open(filename)
+	f := bufio.NewReader(fh)
+
+	if err != nil {
+		return "there was a problem opening the file"
+	}
+	defer fh.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		buf, _, err = f.ReadLine()
+		if err != nil {
+			return "error"
+		}
+
+		s := string(buf)
+		if regex.MatchString(s) {
+			//fmt.Printf("%s\n", string(buf))
+			return s
+		}
+	}
+}
+
+func Replace(re, repl, filename string) {
+	regex, err := regexp.Compile(re)
+	if err != nil {
+		return // there was a problem with the regular expression.
+	}
+
+	fh, err := os.Open(filename)
+	f := bufio.NewReader(fh)
+
+	if err != nil {
+		return // there was a problem opening the file.
+	}
+	defer fh.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		buf, _ , err = f.ReadLine()
+		if err != nil {
+			return
+		}
+
+		s := string(buf)
+		result := regex.ReplaceAllString(s, repl)
+		fmt.Print(result + "\n")
+	}
+}
