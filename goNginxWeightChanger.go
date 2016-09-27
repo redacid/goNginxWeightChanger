ddfcea41cee7db47d363d0e472d523af91935eef
redacid <redacid@redacid.org.ua>
2016-09-27 11:19:26 +0300

    dev

--- goNginxWeightChanger.go
+++ goNginxWeightChanger.go
@@ -134,7 +134,7 @@ func main() {
 			//fmt.Printf("%s\n",NginxServerLine)
 			//fmt.Printf("%s\n",NginxServerNewLine)
 			sshcmd := "/usr/bin/whoami"
-			sshcmd2 := "sed -e \\'/^[ \\t]*#/!s/"+ strings.TrimRight(NginxServerLine,"\r\n") +"/"+ NginxServerNewLine +"/g\\' "+FServer.NginxConfFile
+			sshcmd2 := "sed -e '/^[ \\t]*#/!s/"+ strings.TrimRight(NginxServerLine,"\r\n") +"/"+ NginxServerNewLine +"/g' "+FServer.NginxConfFile
 			fmt.Printf("%s\n",sshcmd2)
 			//fmt.Printf("%s\n",executeCmd(sshcmd, FServer.Name + ":" + strconv.Itoa(FServer.SSHPort), config))
 
