c8daec35a2f5573273516ac0f774d75faa7ccf51
redacid <redacid@redacid.org.ua>
2016-09-27 09:22:51 +0300

    dev

--- goNginxWeightChanger.go
+++ goNginxWeightChanger.go
@@ -128,8 +128,8 @@ func main() {
 			NginxServerLine := "cat "+FServer.NginxConfFile+" | grep -P "+NginxServerRegexp+"| grep "+FServer.Name+" | sed 's/^[ \t]*//' | grep -v ^\"#\" | head -n 1"
 			//newline="server $host weight=$weight max_fails=1 fail_timeout=5; #$comment"
 			//sed -i -e "/^[ \t]*#/!s/$line/$newline/g" ${file}
-			fmt.Printf("%s",NginxServerLine)
-			fmt.Printf("%s",executeCmd(sshcmd, FServer.Name + ":" + strconv.Itoa(FServer.SSHPort), config))
+			fmt.Printf("%s\n",NginxServerLine)
+			fmt.Printf("%s\n",executeCmd(sshcmd, FServer.Name + ":" + strconv.Itoa(FServer.SSHPort), config))
 
 		}
 		//fmt.Printf("%s\n",config.ConfigGlobal.NginxServerString)
