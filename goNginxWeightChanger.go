7179db536ab6f910ff76afd1f790e1e7553831cc
redacid <redacid@redacid.org.ua>
2016-09-29 14:12:06 +0300

    dev

--- goNginxWeightChanger.go
+++ goNginxWeightChanger.go
@@ -205,7 +205,7 @@ func main() {
 				var sshcmd string
 
 				//fmt.Printf("%s-%s:%d\n",BServer.Name,BServer.IP,BServer.SSHPort)
-				fmt.Printf("%s(%s) cpu_load:%d\n", BServer.Name, BServer.IP, GetCpuLoad(BServer.Name))
+				fmt.Printf("%s(%s) %s cpu_load:%d\n", BServer.Name, BServer.IP, BServer.State, GetCpuLoad(BServer.Name))
 
 				if BServer.State == "low" {
 					BackendServerNewWeight = 1
