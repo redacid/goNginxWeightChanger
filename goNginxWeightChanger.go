257d051117887b4cb75206c9c54b6bea46eccfb6
redacid <redacid@redacid.org.ua>
2016-09-29 14:48:03 +0300

    dev

--- goNginxWeightChanger.go
+++ goNginxWeightChanger.go
@@ -236,6 +236,7 @@ func main() {
 
 					}
 					fmt.Printf("--- Up Servers(%d): %d\n",BackendUpSeversSummaryLoad, BackendUpSeversCount)
+					BackendUpSeversSummaryLoad = 0
 
 				} else {
 					BackendStateFlag = ""
